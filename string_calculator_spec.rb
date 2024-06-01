3b1c692fba8430a0afb8795ba261a3a5086b9010
Tufail Mehraj <shahtuffey@gmail.com>
2024-06-01 22:35:03 +0530

    support custom delimiters test

--- string_calculator_spec.rb
+++ string_calculator_spec.rb
@@ -20,4 +20,9 @@ RSpec.describe StringCalculator do
     calculator = StringCalculator.new
     expect(calculator.add('1,2')).to eq(3)
   end
+
+  it 'supports custom delimiters' do
+    calculator = StringCalculator.new
+    expect(calculator.add('//;\n1;2')).to eq(3)
+  end
 end
