1df1cf280e1885a2d6a027f2e8fb9720c2367436
Tufail Mehraj <shahtuffey@gmail.com>
2024-06-01 22:25:50 +0530

    empty stirng test case

--- string_calculator_spec.rb
+++ string_calculator_spec.rb
@@ -1,1 +1,11 @@
 # initialise test spec file
+
+require 'rspec'
+require_relative 'string_calculator'
+
+RSpec.describe StringCalculator do
+  it 'returns 0 for an empty string' do
+    calculator = StringCalculator.new
+    expect(calculator.add('')).to eq(0)
+  end
+end
