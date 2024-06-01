bb1942e2eaa2daa81db9924e91571f22b92c7539
Tufail Mehraj <shahtuffey@gmail.com>
2024-06-01 22:31:42 +0530

    delimiter and single digit string

--- string_calculator.rb
+++ string_calculator.rb
@@ -3,8 +3,16 @@ class StringCalculator
   
     def add(numbers)
       return 0 if numbers.empty?
-  
+      delimiters = get_delimiters(numbers)
+      numbers_array = numbers.split(Regexp.union(delimiters))
       
+    end
+    
+    private
+
+    def get_delimiters(numbers)
+      custom_delimiter_match = numbers.match(/\/\/(.*)\n/)
+      custom_delimiter_match ? custom_delimiter_match[1].split('') : [',', '\n']
     end
   end
   
--- string_calculator_spec.rb
+++ string_calculator_spec.rb
@@ -4,8 +4,15 @@ require 'rspec'
 require_relative 'string_calculator'
 
 RSpec.describe StringCalculator do
+    
   it 'returns 0 for an empty string' do
     calculator = StringCalculator.new
     expect(calculator.add('')).to eq(0)
+  end
+
+
+  it 'returns the single number' do
+    calculator = StringCalculator.new
+    expect(calculator.add('1')).to eq(1)
   end
 end
