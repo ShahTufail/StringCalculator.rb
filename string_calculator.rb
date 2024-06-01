14d515655a8c8b8d34f0c43d38268b48046cbc00
Tufail Mehraj <shahtuffey@gmail.com>
2024-06-01 22:36:43 +0530

    number array regex

--- string_calculator.rb
+++ string_calculator.rb
@@ -3,16 +3,26 @@ class StringCalculator
   
     def add(numbers)
       return 0 if numbers.empty?
+  
       delimiters = get_delimiters(numbers)
       numbers_array = numbers.split(Regexp.union(delimiters))
-      
+  
+      check_for_negatives(numbers_array)
+  
+      numbers_array.reduce(0) do |sum, current|
+        num = current.to_i
+        next sum if num > MAX_SAFE_INTEGER
+        sum + num
+      end
     end
-    
+  
     private
-
+  
     def get_delimiters(numbers)
       custom_delimiter_match = numbers.match(/\/\/(.*)\n/)
       custom_delimiter_match ? custom_delimiter_match[1].split('') : [',', '\n']
     end
+  
+    
   end
   
