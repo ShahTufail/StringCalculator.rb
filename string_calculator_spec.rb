91cac02f311f14b05608215f0190e0b539a3784d
Tufail Mehraj <shahtuffey@gmail.com>
2024-06-01 22:33:06 +0530

    multiple numbers separated by comma test

--- string_calculator_spec.rb
+++ string_calculator_spec.rb
@@ -4,7 +4,7 @@ require 'rspec'
 require_relative 'string_calculator'
 
 RSpec.describe StringCalculator do
-    
+
   it 'returns 0 for an empty string' do
     calculator = StringCalculator.new
     expect(calculator.add('')).to eq(0)
@@ -14,5 +14,10 @@ RSpec.describe StringCalculator do
   it 'returns the single number' do
     calculator = StringCalculator.new
     expect(calculator.add('1')).to eq(1)
+  end
+
+  it 'adds multiple numbers separated by comma' do
+    calculator = StringCalculator.new
+    expect(calculator.add('1,2')).to eq(3)
   end
 end
